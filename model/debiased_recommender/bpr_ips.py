Fix loss calculation

model/debiased_recommender/bpr_ips.py
@@ -38,7 +38,7 @@ class BPR_IPS(DebiasedRecommender):
         # define layers and loss
         self.user_embedding = nn.Embedding(self.n_users, self.embedding_size)
         self.item_embedding = nn.Embedding(self.n_items, self.embedding_size)
-        self.loss = BPRLoss()
+        self.loss = BPRLoss(reduction="none")
 
         self.propensity_score, self.column = dataset.estimate_pscore()
         self.use_precomp_prop = config["use_precomp_prop"]

model/debiased_recommender/lightgcn_ips.py
@@ -70,8 +70,10 @@ class LightGCN_IPS(DebiasedRecommender):
         self.item_embedding = torch.nn.Embedding(
             num_embeddings=self.n_items, embedding_dim=self.latent_dim
         )
-        self.mf_loss = BPRLoss()
-        self.reg_loss = EmbLoss()
+        self.mf_loss = BPRLoss(reduction="none")
+        self.reg_loss = (
+            EmbLoss()
+        )  # this returns only one thing when we need a matrix for the IPS
 
         # storage variables for full sort evaluation acceleration
         self.restore_user_e = None

model/debiased_recommender/neumf_ips.py
@@ -71,7 +71,7 @@ class NeuMF_IPS(DebiasedRecommender):
         elif self.mlp_train:
             self.predict_layer = nn.Linear(self.mlp_hidden_size[-1], 1)
         self.sigmoid = nn.Sigmoid()
-        self.loss = nn.BCEWithLogitsLoss()
+        self.loss = nn.BCEWithLogitsLoss(reduction="none")
 
         self.propensity_score, self.column = dataset.estimate_pscore()
 
@@ -153,7 +153,9 @@ class NeuMF_IPS(DebiasedRecommender):
         weight = self.propensity_score.to(self.device)[
             interaction[self.column].long()
         ].to(self.device)
-        return loss
+
+        loss = self.loss(output, label)
+        return torch.mean(1 / (weight + 1e-7) * loss)
 
     def predict(self, interaction):
         user = interaction[self.USER_ID]

model/general_recommender/neumf.py
@@ -96,12 +96,12 @@ class NeuMF(GeneralRecommender):
             if isinstance(layer, nn.Linear):
                 weight_key = "mlp_layers." + mlp_layers[index]
                 bias_key = "mlp_layers." + mlp_layers[index + 1]
-                assert (
-                    layer.weight.shape == mlp[weight_key].shape
-                ), f"mlp layer parameter shape mismatch"
-                assert (
-                    layer.bias.shape == mlp[bias_key].shape
-                ), f"mlp layer parameter shape mismatch"
+                assert layer.weight.shape == mlp[weight_key].shape, (
+                    f"mlp layer parameter shape mismatch"
+                )
+                assert layer.bias.shape == mlp[bias_key].shape, (
+                    f"mlp layer parameter shape mismatch"
+                )
                 layer.weight.data.copy_(mlp[weight_key])
                 layer.bias.data.copy_(mlp[bias_key])
                 index += 2
@@ -147,7 +147,8 @@ class NeuMF(GeneralRecommender):
         label = interaction[self.LABEL]
 
         output = self.forward(user, item)
-        return self.loss(output, label)
+        loss = self.loss(output, label)
+        return loss
 
     def predict(self, interaction):
         user = interaction[self.USER_ID]

model/loss.py
@@ -38,13 +38,16 @@ class BPRLoss(nn.Module):
         >>> output.backward()
     """
 
-    def __init__(self, gamma=1e-10):
+    def __init__(self, gamma=1e-10, reduction="mean"):
         super(BPRLoss, self).__init__()
         self.gamma = gamma
+        self.reduction = reduction
 
     def forward(self, pos_score, neg_score):
-        loss = -torch.log(self.gamma + torch.sigmoid(pos_score - neg_score)).mean()
-        return loss
+        loss = torch.log(self.gamma + torch.sigmoid(pos_score - neg_score))
+        if self.reduction == "none":
+            return -loss
+        return -loss.mean()
 
 
 class RegLoss(nn.Module):