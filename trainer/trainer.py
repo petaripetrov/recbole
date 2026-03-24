b3dc7deb5b3f2f3b1f2c7b127f4e47b46e72eee8
Petar Petrov <petizad55@gmail.com>
2026-03-24 13:00:31 +0100

    more fair stuff

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -2388,3 +2388,193 @@ class PFCN_PMFTrainer(PFCNTrainer):
                                                         + [{'params':self.model.item_embedding_layer.weight}]
                                                         + [{'params':_.parameters()} for _ in model.filter_layer.values()])
             self.optimizer_dis = self._build_optimizer(params=[{'params':_.parameters()} for _ in model.dis_layer_dict.values()])
+
+
+class FAiRTrainer(Trainer):
+    def __init__(self, config, model):
+        super(FAiRTrainer, self).__init__(config, model)
+        
+        self.pretrain_epochs = 10
+        
+    def _run_pretrain(self, train_data, idx, verbose, show_progress):
+        r"""Run pretraining step
+        """
+        start = time()
+        
+        loss = self._train_epoch(train_data, idx, show_progress=show_progress)
+        
+        end = time()
+        
+        train_loss_output = self._generate_train_loss_output(
+            idx, start, end, loss
+        )
+        
+        if verbose:
+            self.logger.info(train_loss_output)
+            
+    def _run_valid(self, data, verbose, show_progress):
+        valid_start_time = time()
+        valid_score, valid_result = self._valid_epoch(
+            data, show_progress=show_progress
+        )
+
+        (
+            self.best_valid_score,
+            self.cur_step,
+            stop_flag,
+            update_flag,
+        ) = early_stopping(
+            valid_score,
+            self.best_valid_score,
+            self.cur_step,
+            max_step=self.stopping_step,
+            bigger=self.valid_metric_bigger,
+        )
+        valid_end_time = time()
+        valid_score_output = (
+            set_color("epoch %d evaluating", "green")
+            + " ["
+            + set_color("time", "blue")
+            + ": %.2fs, "
+            + set_color("valid_score", "blue")
+            + ": %f]"
+        ) % (9, valid_end_time - valid_start_time, valid_score)
+        valid_result_output = (
+            set_color("valid result", "blue") + ": \n" + dict2str(valid_result)
+        )
+        if verbose:
+            self.logger.info(valid_score_output)
+            self.logger.info(valid_result_output)
+            
+        
+    def fit(
+        self,
+        train_data,
+        valid_data=None,
+        verbose=True,
+        saved=True,
+        show_progress=False,
+        callback_fn=None
+    ):
+        r"""Train the model based on the train data and the valid data. Modified to run 10 pretrain epochs 
+            before continuing as recbole typically does
+
+        Args:
+            train_data (DataLoader): the train data
+            valid_data (DataLoader, optional): the valid data, default: None.
+                                               If it's None, the early_stopping is invalid.
+            verbose (bool, optional): whether to write training and evaluation information to logger, default: True
+            saved (bool, optional): whether to save the model parameters, default: True
+            show_progress (bool): Show the progress of training epoch and evaluate epoch. Defaults to ``False``.
+            callback_fn (callable): Optional callback function executed at end of epoch.
+                                    Includes (epoch_idx, valid_score) input arguments.
+
+        Returns:
+             (float, dict): best valid score and best valid result. If valid_data is None, it returns (-1, None)
+        """
+    
+        if saved and self.start_epoch >= self.epochs:
+            self._save_checkpoint(-1, verbose=verbose)
+            
+        self.eval_collector.data_collect(train_data)
+        
+        if self.config["equalize_attention"]:
+            # TODO move this to __init__
+            self.protected_map = train_data.dataset.protected_map
+            
+        self.avg_rating = train_data.dataset.avg_rating
+        
+        if self.config["train_neg_sample_args"].get("dynamic", False):
+            train_data.get_model(self.get_model())
+        valid_step = 0
+        
+        # for pretrain_idx in range(self.start_epoch, self.pretrain_epochs):
+            # self._run_pretrain(train_data, pretrain_idx, verbose, show_progress)
+            # self._run_valid(valid_data, verbose, show_progress)
+            
+        self.model.is_pretrained = True
+            
+        for epoch_idx in range(self.start_epoch, self.epochs):
+            # train
+            training_start_time = time()
+            train_loss = self._train_epoch(
+                train_data, epoch_idx, show_progress=show_progress
+            )
+            self.train_loss_dict[epoch_idx] = (
+                sum(train_loss) if isinstance(train_loss, tuple) else train_loss
+            )
+            training_end_time = time()
+            train_loss_output = self._generate_train_loss_output(
+                epoch_idx, training_start_time, training_end_time, train_loss
+            )
+            if verbose:
+                self.logger.info(train_loss_output)
+            self._add_train_loss_to_tensorboard(epoch_idx, train_loss)
+            self.wandblogger.log_metrics(
+                {"epoch": epoch_idx, "train_loss": train_loss, "train_step": epoch_idx},
+                head="train",
+            )
+
+            # eval
+            if self.eval_step <= 0 or not valid_data:
+                if saved:
+                    self._save_checkpoint(epoch_idx, verbose=verbose)
+                continue
+            if (epoch_idx + 1) % self.eval_step == 0:
+                valid_start_time = time()
+                valid_score, valid_result = self._valid_epoch(
+                    valid_data, show_progress=show_progress
+                )
+
+                (
+                    self.best_valid_score,
+                    self.cur_step,
+                    stop_flag,
+                    update_flag,
+                ) = early_stopping(
+                    valid_score,
+                    self.best_valid_score,
+                    self.cur_step,
+                    max_step=self.stopping_step,
+                    bigger=self.valid_metric_bigger,
+                )
+                valid_end_time = time()
+                valid_score_output = (
+                    set_color("epoch %d evaluating", "green")
+                    + " ["
+                    + set_color("time", "blue")
+                    + ": %.2fs, "
+                    + set_color("valid_score", "blue")
+                    + ": %f]"
+                ) % (epoch_idx, valid_end_time - valid_start_time, valid_score)
+                valid_result_output = (
+                    set_color("valid result", "blue") + ": \n" + dict2str(valid_result)
+                )
+                if verbose:
+                    self.logger.info(valid_score_output)
+                    self.logger.info(valid_result_output)
+                self.tensorboard.add_scalar("Vaild_score", valid_score, epoch_idx)
+                self.wandblogger.log_metrics(
+                    {**valid_result, "valid_step": valid_step}, head="valid"
+                )
+
+                if update_flag:
+                    if saved:
+                        self._save_checkpoint(epoch_idx, verbose=verbose)
+                    self.best_valid_result = valid_result
+
+                if callback_fn:
+                    callback_fn(epoch_idx, valid_score)
+
+                if stop_flag:
+                    stop_output = "Finished training, best eval result in epoch %d" % (
+                        epoch_idx - self.cur_step * self.eval_step
+                    )
+                    if verbose:
+                        self.logger.info(stop_output)
+                    break
+
+                valid_step += 1
+
+        self._add_hparam_to_tensorboard(self.best_valid_score)
+        return self.best_valid_score, self.best_valid_result
